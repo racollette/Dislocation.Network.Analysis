203528ffe7b1b8245ec9fb2b186a505494b02a2f
Ryan Collette <racollette@gmail.com>
2019-12-19 12:01:31 -0700

    update

--- python/cluster.py
+++ python/cluster.py
@@ -1,7 +1,7 @@
 from ovito.io import import_file
 from ovito.modifiers import ClusterAnalysisModifier
 import numpy
-
+##
 node = import_file("simulation.dump")
 node.modifiers.append(ClusterAnalysisModifier(cutoff = 2.8, sort_by_size = True))
 node.compute()
